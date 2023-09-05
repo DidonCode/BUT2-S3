9f5064c92a17963dd128e4d052f44a10203e62f9
DidonCode <76182682+DidonCode@users.noreply.github.com>
2023-09-05 19:35:21 +0200

    Ajout de la connexion mysql
    Ajout du system pour connecter le php avec le base de données mysql

--- php/database.php
+++ php/database.php
@@ -1,3 +1,13 @@
 <?php
-    echo "Je suis la";
+	$address = "127.0.0.1";
+	$username = "root";
+	$password = "";
+	$database = "insta_hess";
+
+	try {
+		$pdo = new PDO("mysql:host=".$address.";dbname=".$database, $username, $password);
+		return $pdo;
+	}catch(Exception $e) {
+		die('Erreur : '.$e->getMessage());
+	}
 ?>
