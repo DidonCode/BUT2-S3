d6943d5d36620ec6095eb7cf092b698cc85a2ef0
DidonCode <76182682+DidonCode@users.noreply.github.com>
2023-09-12 21:49:10 +0200

    Upload de la base de donnée

--- insta_hess.sql
+++ insta_hess.sql
@@ -1,48 +1,43 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- version 3.5.1
+-- http://www.phpmyadmin.net
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 07 sep. 2023 à 08:09
--- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
+-- Client: localhost
+-- Généré le: Mar 12 Septembre 2023 à 19:43
+-- Version du serveur: 5.5.16-log
+-- Version de PHP: 5.3.13
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE DATABASE IF NOT EXISTS insta_hess;
+USE insta_hess;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Base de données : `insta_hess`
+-- Base de données: `insta_hess`
 --
-CREATE DATABASE IF NOT EXISTS insta_hess DEFAULT CHARACTER SET utf8mb4 ;
-USE insta_hess;
+
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `account`
 --
 
-DROP TABLE IF EXISTS `account`;
 CREATE TABLE IF NOT EXISTS `account` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `pseudo` varchar(255) NOT NULL,
+  `fullName` varchar(255) NOT NULL,
+  `grade` int(11) NOT NULL DEFAULT '0',
+  `profil` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 ;
-
---
--- Déchargement des données de la table `account`
---
-
-INSERT INTO `account` (`id`, `email`, `password`, `pseudo`) VALUES
-(3, '', '', '');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -50,14 +45,14 @@ INSERT INTO `account` (`id`, `email`, `password`, `pseudo`) VALUES
 -- Structure de la table `comment`
 --
 
-DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `post` int NOT NULL,
-  `publisher` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post` int(11) NOT NULL,
+  `publisher` int(11) NOT NULL,
   `message` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -65,12 +60,24 @@ CREATE TABLE IF NOT EXISTS `comment` (
 -- Structure de la table `follow`
 --
 
-DROP TABLE IF EXISTS `follow`;
 CREATE TABLE IF NOT EXISTS `follow` (
-  `id` int NOT NULL,
-  `follower` int NOT NULL,
-  `followed` int NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ;
+  `id` int(11) NOT NULL,
+  `follower` int(11) NOT NULL,
+  `followed` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `like`
+--
+
+CREATE TABLE IF NOT EXISTS `like` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post` int(11) NOT NULL,
+  `account` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -78,16 +85,15 @@ CREATE TABLE IF NOT EXISTS `follow` (
 -- Structure de la table `message`
 --
 
-DROP TABLE IF EXISTS `message`;
 CREATE TABLE IF NOT EXISTS `message` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `sender` int NOT NULL,
-  `receiver` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender` int(11) NOT NULL,
+  `receiver` int(11) NOT NULL,
   `message` varchar(255) NOT NULL,
   `date` varchar(255) NOT NULL,
   `view` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -95,17 +101,18 @@ CREATE TABLE IF NOT EXISTS `message` (
 -- Structure de la table `post`
 --
 
-DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `publisher` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `publisher` int(11) NOT NULL,
+  `spot` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `contentType` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `photo` varchar(255) NOT NULL,
   `date` varchar(255) NOT NULL,
-  `like` int NOT NULL,
+  `like` int(11) NOT NULL,
+  `enableComment` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ;
-COMMIT;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- message.php
+++ message.php
@@ -3,6 +3,7 @@
 	<head>
 		<title></title>
 		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/message.css">
 		<link rel="stylesheet" type="text/css" href="css/side-bar.css">
 	</head>
 
@@ -11,6 +12,9 @@
 			<?php 
 				include_once("php/side-bar.php");
 			?>
+			<div class="container">
+				
+			</div>
 		</div>
 	</body>
 </html>
