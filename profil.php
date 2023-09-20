f1c274c3afa08b922ccb3e4b63bf063500fef393
Yanom <65492446+II-THEOY78-II@users.noreply.github.com>
2023-09-20 18:55:04 +0200

    Ajout du php de la page profil
    Le précédent commit à cet effet était un miss play de ma part

--- profil.php
+++ profil.php
@@ -1,3 +1,57 @@
+<?php
+
+    require('php/database.php');
+    include_once('php/session.php');
+    
+    if(valid()){
+
+    
+    $requete = $pdo->prepare("SELECT pseudo FROM account WHERE id =?");
+    $requete->execute(array($_SESSION['id']));
+
+    $pseudo = $requete->fetch();
+
+    $requete = $pdo-> prepare("SELECT id, pseudo, fullName, bio, profil FROM account WHERE id = ?");
+        $requete->execute(array($_SESSION['id']));
+        
+        $profilData = $requete->fetchAll();
+
+
+        $id = $profilData[0]['id'];
+        $pseudo = $profilData[0]['pseudo'];
+        $fullName = $profilData[0]['fullName'];
+        $bio = $profilData[0]['bio'];
+        $PDP = $profilData[0]['profil'];
+    }
+
+    if(isset($_GET['profil'])){
+        $requete = $pdo-> prepare("SELECT id, pseudo, fullName, bio, profil FROM account WHERE pseudo = ?");
+        $requete->execute(array($_GET['profil']));
+        
+        $profilData = $requete->fetchAll();
+            // destroySession();
+    }
+
+
+    $requete = $pdo->prepare("SELECT id, spot, content, contentType, description, date, enableComment FROM post WHERE publisher =?");
+    $requete->execute(array($id));
+
+    $postData = $requete->fetchAll();
+
+    $postNumber = $requete->rowcount();
+
+    
+    
+    
+
+    // var_dump($postData);
+
+
+
+
+    
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,24 +62,57 @@
 </head>
 
 <body>
+
+    <?php 
+        if(!empty($profilData)){
+        
+            $id = $profilData[0]['id'];
+            $pseudo = $profilData[0]['pseudo'];
+            $fullName = $profilData[0]['fullName'];
+            $bio = $profilData[0]['bio'];
+            $PDP = $profilData[0]['profil'];
+        }
+        else{
+            include_once("php/side-bar.php");
+            echo "Désolé nous ne trouvons pas ce profil.";
+            exit();
+        }
+    ?>
 <div class="main-container">
-            <?php 
-				include_once("php/side-bar.php");
-			?>
+    <?php 
+        include_once("php/side-bar.php");
+    ?>
     <div class="container">
+        <div id="settings">
+            <div id="menuButton" class="svg-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                    <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"/>
+                </svg>
+            </div>
+            <div id="contextMenu" class="context-menu">
+                <ul>
+                    <li><a href="#">Modifier le nom</a></li>
+                    <li><a href="#">Modifier la bio</a></li>
+                    <li><a href="#">Modifier la photo de profil</a></li>
+                    <li><a href="#">Changer le mot de passe</a></li>
+                    <li><a href="#">Compte bloqué</a></li>
+                </ul>
+            </div>
+        </div>
         <div id="bio">
             <div class="photo-profil">
-                <img src="images/profil/profil-instahess.png" alt="photo-profil">
+                <?php echo '<img src="'.$PDP. '" alt="photo-profil">'; ?>
             </div>
             <div class="profil-info">
                 <ul class="informations">
                     <li class="settings">
                        <div class = "certif">
-                            <p class="username">Mango_Manga</p>
+                            <p class="username"><?php echo $pseudo; ?></p>
                             <svg style="position: relative; left:10px;" color="rgb(0, 149, 246)" fill="rgb(0, 149, 246)" height="18" role="img" viewBox="0 0 40 40" width="18">
                                 <path d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Zm7.415 11.225 2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z" fill-rule="evenodd"></path>
                             </svg>
-                        </div> 
+                        </div>
+                        <br>    
                         <button id="followButton" class="follow-button" onclick="toggleFollow()">Follow</button>
                     </li>
                     <li><span>0</span> Post</li>
@@ -33,25 +120,32 @@
                     <li><span>0</span> Suivi</li>
                 </ul>
 				<div class="texte-bio">
-					<p>Salut c'est Natoo tu veux un iphone 15 pro max ? envois moi les codes quand tu appels le 3630 ! </p>
+					<p><?php echo $bio; ?></p>
 				</div>
             </div>
         </div>
-    </div>
-	<div id="settings">
-        <div id="menuButton" class="svg-button">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
-                <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"/>
-            </svg>
-        </div>
-        <div id="contextMenu" class="context-menu">
-            <ul>
-                <li><a href="#">Modifier le nom</a></li>
-                <li><a href="#">Modifier la bio</a></li>
-                <li><a href="#">Modifier la photo de profil</a></li>
-                <li><a href="#">Changer le mot de passe</a></li>
-                <li><a href="#">Compte bloqué</a></li>
-            </ul>
+        <div class="AffichagePost">
+            <?php 
+            for($i=0; $i<$postNumber; $i++){
+                $postId = $postData[$i]['id'];
+                $postSpot = $postData[$i]['spot'];
+                $postContent = $postData[$i]['content'];
+                $postContentType = $postData[$i]['contentType'];
+                $postDescription = $postData[$i]['description'];
+                $postDate = $postData[$i]['date'];
+                $postEnableComment = $postData[$i]['enableComment'];
+            
+                if($postContentType == "image"){
+                    echo '<img src="' . $postContent . '">'; 
+                }
+                else{
+                    echo '<video muted autoplay loop>
+							<source src="'.$postContent.'" type="video/mp4"/>
+					</video>';
+                }
+            }    
+            ?>
+
         </div>
     </div>
 </div>
@@ -88,7 +182,7 @@
 
         contextMenu.addEventListener("click", function (event) {
             event.stopPropagation();
-        }); 
+        });
     });
 </script>
 
