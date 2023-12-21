d900226671bf5663d5f3cc8373bd774b3682fc33
Killian.b <killian2282@gmail.com>
2023-12-21 11:21:00 +0100

    page settings (fini)

--- settings.php
+++ settings.php
@@ -12,53 +12,78 @@
 	<?php 
 		include_once("php/side-bar.php");
 	?>
-<div class="container">
-    <div class="general">
-        <h1>GÉNÉRAL</h1>
-        <div id="changePseudo">
-            <h3>Changer de pseudo</h3>
-            <input type="text" placeholder="Nouveau pseudo">
-        </div>
-    
+  <div class="panel">
+    <div class="header">
+      <h2>PHOTO DE PROFIL</h2>
+    </div>
+    <div class="content">
+      <div class="profile-section">
+        <div class="profile-image">
+          <img src="images/profil/civilisation.jpg" alt="">
+        </div> <br>
+        <input type="file" id="fileInput" accept="image/*"> <br>
+        <button class="apply-btn">Enregistrer la sélection</button>
+      </div>
+    </div>
+  </div>
 
-    <div id="changeName">
-        <h3>Changer de nom</h3>
-        <div id="nameSubMenu">
-            <input type="text" placeholder="Nouveau nom">
-        </div>
+  <div class="panel">
+    <div class="header">
+      <h2>INFORMATIONS</h2>
+    </div>
+    <div class="content">
+      <div class="info-section">
+        <h3>Modifier les informations</h3>
+
+        <div class="form__group change-input">
+        <input type="input" class="form__field" placeholder="" name="newPseudo" id="newPseudo" required />
+        <label for="newPseudo" class="form__label">Nouveau pseudo</label>
     </div>
 
-    <div id="changePhoto">
-        <h3>Changer photo de profil</h3>
-        <div id="photoSubMenu">
-            <input type="file" accept="image/*">
-        </div>
+    <div class="form__group change-input">
+        <input type="input" class="form__field" placeholder="" name="newName" id="newName" required />
+        <label for="newName" class="form__label">Nouveau nom</label>
+    </div>
+        
+    <div class="form__group change-input">
+        <textarea class="form__field" placeholder="" name="newBio" id="newBio" required></textarea>
+        <label for="newBio" class="form__label">Nouvelle bio</label>
     </div>
 
-    <div id="changeBio">
-        <h3>Modifier la bio</h3>
-        <div id="bioSubMenu">
-            <textarea placeholder="Nouvelle bio"></textarea>
-        </div>
+        <button class="apply-btn">Enregistrer la sélection</button>
+      </div>
     </div>
+  </div>
+  
+  <div class="panel">
+    <div class="header">
+      <h2>SÉCURITÉ</h2>
     </div>
-    <div class="securite">
-    <h1>SÉCURITÉ</h1>
-    <div id="changePassword">
-        <h3>Changer de mot de passe</h3>
-            <input type="password" placeholder="Nouveau mot de passe">
+    <div class="content">
+      <div class="info-section">
+      <div class="form__group change-input">
+        <input type="password" class="form__field" placeholder="" name="newPassword" id="newPassword" required />
+        <label for="newPassword" class="form__label">Nouveau mot de passe</label>
     </div>
+        <button class="apply-btn">Enregistrer la sélection</button>
+      </div>
     </div>
-    <div class="confidentialite">
-    <h1>CONFIDENTIALITÉ</h1>
-    <div id="blockedAccounts">
+  </div>
+  
+  <div class="panel">
+    <div class="header">
+      <h2>CONFIDENTIALITÉ</h2>
+    </div>
+    <div class="content">
+      <div class="info-section">
+      <div id="blockedAccounts">
         <h3>Comptes bloqués</h3>
         <p>Aucun compte bloqué pour le moment.</p>
     </div>
+        <button class="apply-btn">Enregistrer la sélection</button>
+      </div>
     </div>
-    <button class="apply-btn" onclick="applyChanges()">Appliquer les modifications</button>
-</div>
-</div>
+  </div>
     <script src="js/settings.js"></script>
     <script src="js/side-bar.js"></script>
 </body>
