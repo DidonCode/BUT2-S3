08180916baa6e7b9a1bfb0433ef217b5bb5e98ab
Killian.b <killian2282@gmail.com>
2023-12-21 13:19:41 +0100

    restructuration CSS settings

--- settings.php
+++ settings.php
@@ -9,81 +9,84 @@
 </head>
 <body>
 <div class="main-container">
+    <div class="container">
 	<?php 
 		include_once("php/side-bar.php");
 	?>
-  <div class="panel">
-    <div class="header">
+  <div class="settings-panel">
+    <div class="settings-header">
       <h2>PHOTO DE PROFIL</h2>
     </div>
-    <div class="content">
-      <div class="profile-section">
-        <div class="profile-image">
+    <div class="settings-content">
+      <div class="settings-profile-section">
+        <div class="settings-profile-image">
           <img src="images/profil/civilisation.jpg" alt="">
         </div> <br>
         <input type="file" id="fileInput" accept="image/*"> <br>
-        <button class="apply-btn">Enregistrer la sélection</button>
+        <button class="settings-apply-btn">Enregistrer la sélection</button>
       </div>
     </div>
   </div>
 
-  <div class="panel">
-    <div class="header">
+  <div class="settings-panel">
+    <div class="settings-header">
       <h2>INFORMATIONS</h2>
     </div>
-    <div class="content">
-      <div class="info-section">
+    <div class="settings-content">
+      <div class="settings-info-section">
         <h3>Modifier les informations</h3>
 
-        <div class="form__group change-input">
-        <input type="input" class="form__field" placeholder="" name="newPseudo" id="newPseudo" required />
-        <label for="newPseudo" class="form__label">Nouveau pseudo</label>
+        <div class="settings-form-group settings-change-input">
+        <input type="input" class="settings-form-field" placeholder="" name="newPseudo" id="newPseudo" required />
+        <label for="newPseudo" class="settings-form-label">Nouveau pseudo</label>
     </div>
 
-    <div class="form__group change-input">
-        <input type="input" class="form__field" placeholder="" name="newName" id="newName" required />
-        <label for="newName" class="form__label">Nouveau nom</label>
+    <div class="settings-form-group settings-change-input">
+        <input type="input" class="settings-form-field" placeholder="" name="newName" id="newName" required />
+        <label for="newName" class="settings-form-label">Nouveau nom</label>
     </div>
         
-    <div class="form__group change-input">
-        <textarea class="form__field" placeholder="" name="newBio" id="newBio" required></textarea>
-        <label for="newBio" class="form__label">Nouvelle bio</label>
+    <div class="settings-form-group settings-change-input">
+        <textarea class="settings-form-field" placeholder="" name="newBio" id="newBio" required></textarea>
+        <label for="newBio" class="settings-form-label">Nouvelle bio</label>
     </div>
 
-        <button class="apply-btn">Enregistrer la sélection</button>
+        <button class="settings-apply-btn">Enregistrer la sélection</button>
       </div>
     </div>
   </div>
   
-  <div class="panel">
-    <div class="header">
+  <div class="settings-panel">
+    <div class="settings-header">
       <h2>SÉCURITÉ</h2>
     </div>
-    <div class="content">
-      <div class="info-section">
-      <div class="form__group change-input">
-        <input type="password" class="form__field" placeholder="" name="newPassword" id="newPassword" required />
-        <label for="newPassword" class="form__label">Nouveau mot de passe</label>
+    <div class="settings-content">
+      <div class="settings-info-section">
+      <div class="settings-form-group settings-change-input">
+        <input type="password" class="settings-form-field" placeholder="" name="newPassword" id="newPassword" required />
+        <label for="newPassword" class="settings-form-label">Nouveau mot de passe</label>
     </div>
-        <button class="apply-btn">Enregistrer la sélection</button>
+        <button class="settings-apply-btn">Enregistrer la sélection</button>
       </div>
     </div>
   </div>
   
-  <div class="panel">
-    <div class="header">
+  <div class="settings-panel">
+    <div class="settings-header">
       <h2>CONFIDENTIALITÉ</h2>
     </div>
-    <div class="content">
-      <div class="info-section">
-      <div id="blockedAccounts">
+    <div class="settings-content">
+      <div class="settings-info-section">
+      <div id="settings-blockedAccounts">
         <h3>Comptes bloqués</h3>
         <p>Aucun compte bloqué pour le moment.</p>
     </div>
-        <button class="apply-btn">Enregistrer la sélection</button>
+        <button class="settings-apply-btn">Enregistrer la sélection</button>
       </div>
     </div>
   </div>
+</div>
+</div>
     <script src="js/settings.js"></script>
     <script src="js/side-bar.js"></script>
 </body>
